edf33314b381f8c58123fd5d4ceceae2d9c24d72
Jorge Aguilera <jorge.aguilera@puravida-software.com>
2016-06-04 13:51:55 +0200

    Update 2016-06-04T11:51:53.566Z

--- bower_components/js-xlsx/tests/InterviewSpec.js
+++
@@ -1,8 +0,0 @@
-var XLSX = require('../');
-var testCommon = require('./Common.js');
-
-var file = 'interview.xlsx';
-
-describe(file, function () {
-	testCommon(file);
-});
--- bower_components/js-xlsx/tests/IssueSpec.js
+++
@@ -1,8 +0,0 @@
-var XLSX = require('../');
-var testCommon = require('./Common.js');
-
-var file = 'issue.xlsx';
-
-describe(file, function () {
-	testCommon(file);
-});
